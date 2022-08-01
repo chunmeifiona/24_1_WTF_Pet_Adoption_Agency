3c6989478e2dcd9037871c56390addd923e43920
Fiona <chunmei.fiona.li@gmail.com>
2022-08-01 10:57:58 -0500

    fix available feature

--- models.py
+++ models.py
@@ -23,6 +23,6 @@ class Pet(db.Model):
     name=db.Column(db.Text, nullable=False)
     species=db.Column(db.Text, nullable=False)
     photo_url=db.Column(db.Text, default=DEFAULT_PHOTO_URL)
-    age=db.Column(db.Integer, default = 3)
+    age=db.Column(db.Integer)
     notes=db.Column(db.Text)
     available=db.Column(db.Boolean,nullable=False, default=True)
