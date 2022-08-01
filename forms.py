92743061e79f5c527034cdf53d2dc0d7b5977973
Fiona <chunmei.fiona.li@gmail.com>
2022-08-01 10:48:31 -0500

    add edit features

--- forms.py
+++ forms.py
@@ -1,10 +1,18 @@
+from xmlrpc.client import Boolean
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField, SelectField
+from wtforms import StringField, IntegerField, TextAreaField, SelectField, BooleanField
 from wtforms.validators import URL, Optional, NumberRange
 
 class AddPetForm(FlaskForm):
+    """Form for adding pets."""
     name=StringField("Pet name")
     species=SelectField("Species", choices=[('cat','cat'),('dog','dog'),('porcupine','porcupine')])
     photo_url=StringField("Photo URL", validators=[Optional(), URL()])
     age=IntegerField("Age", validators=[NumberRange(min=0, max=30)])
-    notes=TextAreaField("Notes")
+    notes=TextAreaField("Notes", validators=[Optional()])
+
+class EditPetForm(FlaskForm):
+    """Form for editing an existing pet."""
+    photo_url = StringField("Photo URL", validators=[Optional(), URL()])
+    notes = TextAreaField("Notes", validators=[Optional()])
+    avaiable = BooleanField("Avaiable?")
--- models.py
+++ models.py
@@ -1,19 +1,28 @@
-"""Models for Per Adoption Agency"""
+"""Models for Pet Adoption Agency"""
 from flask_sqlalchemy import SQLAlchemy
 
 db=SQLAlchemy()
+DEFAULT_PHOTO_URL = "https://images.unsplash.com/photo-1563460716037-460a3ad24ba9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBldHN8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
 
 def connect_db(app):
+    """
+    Connect this database to provided Flask app.
+    You should call this in your Flask app.
+    """
     db.app=app
     db.init_app(app)
 
 class Pet(db.Model):
+    """Adoptable pet."""
     __tablename__='pets'
+    def __repr__(self):
+        p=self
+        return f"<Pet {p.id} {p.name} {p.photo_url} {p.age} {p.available}>"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name=db.Column(db.Text, nullable=False)
     species=db.Column(db.Text, nullable=False)
-    photo_url=db.Column(db.Text)
-    age=db.Column(db.Integer)
+    photo_url=db.Column(db.Text, default=DEFAULT_PHOTO_URL)
+    age=db.Column(db.Integer, default = 3)
     notes=db.Column(db.Text)
     available=db.Column(db.Boolean,nullable=False, default=True)
